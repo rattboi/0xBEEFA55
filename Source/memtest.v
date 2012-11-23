56ca690a0aee61d21249161819720d9d66a94388
Andy Goetz <andy@andygoetz.org>
2012-11-23 14:06:01 -0800

    added ability to trace testbench if tracefile option is specified. Also added clear and clock signals to testbench

--- Source/memtest.v
+++ Source/memtest.v
@@ -2,20 +2,47 @@
 // command line, and displays the commands in that file
 
 module test();
-   
+
+   parameter IDLE_CLOCKS = 2;
+   parameter CLOCK_CYCLE  = 20;
+   parameter CLOCK_WIDTH  = CLOCK_CYCLE/2;
+   parameter TRUE   = 1'b1;
+   parameter FALSE  = 1'b0;
+
+   reg Clock;
+   reg Clear;
    integer file;		// the file handle 
    
    reg [3:0] command;
    reg [31:0] value;
    reg [9000:0] filename;
+   reg [9000:0] tracefile;
    integer count;
+
+   initial
+     begin
+	Clock = FALSE;
+	forever #CLOCK_WIDTH Clock = ~Clock;
+     end
+
+   
    initial begin
 
+      // If a tracefile has been specified, generate a vcd file
+      if($value$plusargs("tracefile=%s", tracefile)) begin
+	 $dumpfile(tracefile);
+	 $dumpvars();
+	 end
+      
       // Check to make sure that a stimulus file was provided
       if (! $value$plusargs("stimulus=%s", filename)) begin
          $display("ERROR: No Stimulus specified. Please specify +stimulus=<filename> to start.");
          $finish;
       end
+
+	Clear = TRUE;
+	repeat (IDLE_CLOCKS) @(negedge Clock);
+	Clear = FALSE;
 
       // If it was, open the file
       file = $fopenr(filename);
@@ -24,9 +51,11 @@ module test();
       while (count > 0) begin
 	 // Parse the line
 	 count = $fscanf(file, "%d %x", command, value);
-	 if(count > 0)
+	 if(count > 0) begin
 	   // Display each line
-	   $display("command = %d value = %x", command, value);
+	    repeat(1) @(negedge Clock);
+	    $display("command = %d value = %x", command, value);
+	 end
       end
       // Close the file, and finish up
       $fclose(file);
