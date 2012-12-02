ccc0c386ed7a3982f39be019316bd4cf8fe49acf
wookieeCAT <kevinlriedl@gmail.com>
2012-12-02 13:52:26 -0800

    changed loop variables to be way_cnt and line_cnt in INS_CACHE.v

--- BEEFA55/Source/INS_CACHE.v
+++ BEEFA55/Source/INS_CACHE.v
@@ -31,6 +31,8 @@ module INS_CACHE(
 	parameter INST_FETCH 	= 4'd2;
 	parameter PRINT			= 4'd9;
 	
+
+	// initialize counters to zero
 	initial begin
 	hit = 0;
 	miss = 0;
@@ -44,7 +46,7 @@ module INS_CACHE(
 	reg [11:0] 			Tag 	[`LINES-1:0] [`WAYS-1:0];
 	
 	// loop counters
-	integer i,j;
+	integer line_cnt,way_cnt;
 	 
 	// internal
 	reg done = 1'b0;
@@ -53,6 +55,8 @@ module INS_CACHE(
 	wire [11:0] curr_tag = add_in[31:20];
 	wire [13:0] curr_index = add_in[19:6];
 	 
+
+	//
 	always @(posedge clk)
 	begin	
 		add_out = 26'bZ;
@@ -65,25 +69,25 @@ module INS_CACHE(
 				miss 	= 32'b0;
 				reads	= 32'b0;
 				
-				for (i = 0; i < `LINES; i = i+1'b1) 	// for every line
+				for (line_cnt = 0; line_cnt < `LINES; line_cnt = line_cnt + 1'b1) 	// for every line
 				begin
-					LRU[i] = 1'b0;	
-					for (j = 0; j < `WAYS; j = j+1'b1)	// for all ways
+					LRU[line_cnt] = 1'b0;	
+					for (way_cnt = 0; way_cnt < `WAYS; way_cnt = way_cnt+1'b1)	// for all ways
 					begin
-						Valid	[i][j]	= 1'b0;	
-						Tag  	[i][j]	= 24'b0;
+						Valid	[line_cnt][way_cnt]	= 1'b0;	
+						Tag  	[line_cnt][way_cnt]	= 24'b0;
 					end
 				end
 			end
 			
 			INVALIDATE:
 			begin
-				for (j = 0; j < `WAYS; j = j+1'b1)
+				for (way_cnt = 0; way_cnt < `WAYS; way_cnt = way_cnt+1'b1)
 					if (!done)
-						if (Tag[curr_index][j] == curr_tag)
+						if (Tag[curr_index][way_cnt] == curr_tag)
 						begin
 							done = 1'b1;
-							Valid[curr_index][j] = 1'b0;
+							Valid[curr_index][way_cnt] = 1'b0;
 						end
 			end	
 			
@@ -94,12 +98,12 @@ module INS_CACHE(
 				//	look at all (both) ways.  if for either, the tags match
 				//	and the valid bit is set, this is a hit.  on a hit, the 
 				//  if(!done) will evaluate false and execution drops through.
-				for (j = 0; j < `WAYS; j = j+1'b1)
+				for (way_cnt = 0; way_cnt < `WAYS; way_cnt = way_cnt+1'b1)
 				begin
 					if (!done)
-						if (Tag[curr_index][j] == curr_tag && Valid[curr_index][j] == 1'b1)
+						if (Tag[curr_index][way_cnt] == curr_tag && Valid[curr_index][way_cnt] == 1'b1)
 						begin
-							LRU[curr_index] 	= ~j[0]; // is this logic right? (Yes, I think it is, NOW --rattboi)
+							LRU[curr_index] 	= ~way_cnt[0]; // is this logic right? (Yes, I think it is, NOW --rattboi)
 							done 				= 1'b1;
 							hit 				= hit + 1'b1;
 						end
@@ -114,16 +118,16 @@ module INS_CACHE(
 				// look at both ways.  If either is empty (valid == 0) then 
 				// do a read and and put it in the empty way.  If this happens,
 				// done is set true, and execution will drop out of the loop.
-				for (j = 0; j < `WAYS; j = j+1'b1)
+				for (way_cnt = 0; way_cnt < `WAYS; way_cnt = way_cnt+1'b1)
 				begin
 					if (!done)
-						if (Valid[curr_index][j] == 1'b0)
+						if (Valid[curr_index][way_cnt] == 1'b0)
 						begin
 							done 				= 1'b1;
 							add_out				= add_in[31:6]; // is this right?
-							Tag[curr_index][j] 	= curr_tag;
-							Valid[curr_index][j]= 1'b1;
-							LRU[curr_index] 	= ~j[0]; 
+							Tag[curr_index][way_cnt] 	= curr_tag;
+							Valid[curr_index][way_cnt]= 1'b1;
+							LRU[curr_index] 	= ~way_cnt[0]; 
 						end
 				end
 				
@@ -144,15 +148,15 @@ module INS_CACHE(
 				#2
 				$display("\n------- INSTRUCTION CACHE CONTENTS -------");
 				$display(" Index | LRU | V[0]|Tag[0]| V[1]|Tag[1]");
-				for (j = 0;	j < `LINES; j = j+1)
-					if (Valid[j][0] | Valid[j][1])
+				for (way_cnt = 0;	way_cnt < `LINES; way_cnt = way_cnt+1)
+					if (Valid[way_cnt][0] | Valid[way_cnt][1])
 						$display(" %4h  |  %d  |  %d  | %3h  |  %d  | %3h", 
-							j[`LINEBITS-1:0], 
-							LRU[j], 
-							Valid[j][0], 
-							Valid[j][0] ? Tag[j][0] : `TAGBITS'hX, 
-							Valid[j][1], 
-							Valid[j][1] ? Tag[j][1] : `TAGBITS'hX
+							way_cnt[`LINEBITS-1:0], 
+							LRU[way_cnt], 
+							Valid[way_cnt][0], 
+							Valid[way_cnt][0] ? Tag[way_cnt][0] : `TAGBITS'hX, 
+							Valid[way_cnt][1], 
+							Valid[way_cnt][1] ? Tag[way_cnt][1] : `TAGBITS'hX
 						); 
 				$display("--- END OF INSTRUCTION CACHE CONTENTS ----\n");
 			end
