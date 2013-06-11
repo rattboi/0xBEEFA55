0ee036f013ffccdbcb818cbf817f26e6370eb1d3
ekrause <krause.ericm@gmail.com>
2013-06-10 18:14:17 -0700

    ternary all the things

--- Source/cache.sv
+++ Source/cache.sv
@@ -44,109 +44,54 @@ module cache( cacheinterface.slave bus , cacheinterface.master nextlevel);
   state_t state = RESET;
   state_t next  = RESET;
 
+  // advance state on each clock
   always_ff (@ posedge bus.clock or posedge bus.reset)
     state <= (bus.reset) ? RESET : next;
 
+  // next state logics
+  always_comb
+    if (bus.reset)
+    next_st = RESET;  // is this right?
 
-  // internal
-  bool_t done = FALSE;
+    else
+      case(current_st)
+        RESET:          next_st = IDLE;
 
-  // assignments
-  alias curr_tag   = bus.addr[(ADDRBITS-1)-:TAGBITS];
-  alias curr_index = bus.addr[(WORDBITS+LINEBITS-1):WORDBITS];
-  alias curr_set   = bus.addr[(WORDBITS+LINEBITS+SETBITS-1):(WORDBITS+LINEBITS)];
+        IDLE:           next_st = (bus.evict) ? EVICT_CONFLICT :
+                          (bus.request) ? LOOKUP : IDLE;
 
-  always @(posedge bus.clock)
-  begin
-      addr = 'z;  //'// always initialize address out to high-z
-      operation = NOP;    // default to NOP, if a read happens, it will be updated
-      done      = FALSE;  // and set internal done signal to false
+        EVICT_CONFLICT: next_st = ( ( exists(set[curr_set], curr_tag) ) &&
+                          set[curr_set].way[getway(set[curr_set], curr_tag)].dirty ) ?
+                          WRITEBACK : CLEAR_IRQ;
 
-      case(bus.operation)
+        WRITEBACK:      next_st = CLEAR_IRQ;
 
-        // invalidate everything in the cache
-        RESET: invalidateAll();
+        CLEAR_IRQ:      next_st = IDLE;
 
-        // INVALIDATE: use address passed in with invalidate command as an
-        //    index to a given line.  Then, invalidate the line for which the
-        //    stored tag equals the tag passed in add_in.
-        INVALIDATE:
-          for (way_cnt = 0; way_cnt < `WAYS; way_cnt = way_cnt + 1'b1)
-            if (!done)
-              if (Tag[curr_index][way_cnt] == curr_tag)
-              begin
-                done                        = TRUE;
-                Valid[curr_index][way_cnt]  = FALSE;
-              end
+        LOOKUP:
+        EVICT:
+        GET_NEXT:
+        RW:
+      endcase
 
+  // outputs
+  always_comb
+    case(state)
+      RESET:
+      IDLE:
+      EVICT_CONFLICT:
+      WRITEBACK:
+      CLEAR_IRQ:
+      LOOKUP:
+      EVICT:
+      GET_NEXT:
+      RW:
+    endcase
 
-        INST_FETCH:
-        begin
-          reads++; // always increment read count
-
-          // First, look at both lines.  if for either, the tags match
-          //    and the line is valid, then the read was a hit.  done
-          //    is set to true, and execution will drop through the rest
-          //    of the INST_FETCH routine.
-          for (way_cnt = 0; way_cnt < `WAYS; way_cnt = way_cnt + 1'b1)
-          begin
-            if (done == FALSE)
-              if (Tag[curr_index][way_cnt] == curr_tag &&
-                  Valid[curr_index][way_cnt] == TRUE)
-              begin
-                LRU[curr_index] = ~way_cnt[0];
-                hit             = hit + 1'b1;
-                done             = TRUE;
-              end
-            else ;
-          end
-
-          //  at this point, if done is still false, then the fetch was not a hit.
-          if (done == FALSE)
-            miss = miss + 1'b1;
-
-          // Next, look at both lines.  If either is empty then
-          //    do a read and and put result in the empty line, then set
-          //    done to true, and execution will drop through the rest of
-          //    the INST_FETCH routine.
-          for (way_cnt = 0; way_cnt < `WAYS; way_cnt = way_cnt + 1'b1)
-          begin
-            if (done == FALSE)
-              if (Valid[curr_index][way_cnt] == FALSE)
-              begin
-                // set L_NEXT command/address
-                add_out                      = add_in[31:6]; // perform read
-                cmd_out                      = READ_OUT;     // perform read
-
-                Tag[curr_index][way_cnt]     = curr_tag;
-                Valid[curr_index][way_cnt]   = TRUE;
-                LRU[curr_index]              = ~way_cnt[0];
-                done                         = TRUE;
-              end
-          end
-
-            // Reaching this point means an eviction is needed because the
-            //    instruction fetch was a miss, and there was no empty line
-            //    in which to put the incoming read.  So evict the LRU
-            if (done == FALSE)
-              begin
-                // set L_NEXT command/address
-                add_out                              = add_in[31:6]; // perform read
-                cmd_out                              = READ_OUT;     // perform read
-
-                Tag[curr_index][LRU[curr_index]]    = curr_tag;
-                LRU[curr_index]                     = ~LRU[curr_index];
-              end
-      end
-
-          default: ;  // commands this module doesn't respond to
-        endcase
-  end
-
-task invalidateAll();
-  for (int i = 0; i < SETS; i++)
-    for (int j = 0; j < WAYS; j++)
-      set[i].way[j].valid = INVALID;
-endtask
+  task invalidateAll();
+    for (int i = 0; i < SETS; i++)
+      for (int j = 0; j < WAYS; j++)
+        set[i].way[j].valid = INVALID;
+  endtask
 
 endmodule
