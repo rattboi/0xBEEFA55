ef25e499832772f049f69b3b358db04d4bd892cc
ekrause <krause.ericm@gmail.com>
2013-06-10 17:36:21 -0700

    TODO: Bradon (all)

--- Source/cacheinterface.sv
+++ Source/cacheinterface.sv
@@ -5,9 +5,10 @@
 `include "cachepkg.pkg"
 
 interface cacheinterface
-   #( parameter type WORD = logic[7:0], 
+   #( parameter type WORD = logic[7:0],
        parameter type ADDRSPACE = logic[31:0])
-    (input clock);
+    (input clock
+     input reset);
 
     import cachepkg::*;
 
@@ -16,27 +17,29 @@ interface cacheinterface
     WORD data;
 
     // timing signals - 4 phase signals for variable cache latency
-    logic request; 
+    logic request;
     logic valid;
     logic evict;
 
     modport master(
-        output operation, 
-        inout  addr, 
-        inout  data, 
+        output operation,
+        inout  addr,
+        inout  data,
         output request,
         input  valid,
         input  evict,
-        input  clock);
+        input  clock
+        input  reset);
 
     modport slave (
-        input  operation, 
-        inout  addr, 
+        input  operation,
+        inout  addr,
         inout  data,
         input  request,
         output valid,
         output evict,
-        input  clock);
+        input  clock,
+        input  reset);
 
 endinterface
 
